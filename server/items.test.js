39bd559af2168e0c4155a31e8ac2542bf3817c0e
Alex Evans <aevans@quiktrip.com>
2024-03-04 15:19:14 -0600

    Update Item.js
    Add test to test get request for singular item

--- server/items.test.js
+++ server/items.test.js
@@ -7,10 +7,25 @@ describe("Item Tests", () => {
   beforeAll(async () => {
     await seed();
   });
-  test("get users returns correct response", async () => {
+  test("get items returns correct response", async () => {
     const response = await request(app).get("/api/items");
     expect(response.statusCode).toBe(200);
     expect(response.body[0]).toEqual(
+      expect.objectContaining({
+        name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        price: 109.95,
+        description:
+          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        category: "men's clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      })
+    );
+  });
+
+  test("get one item returns correct response", async () => {
+    const response = await request(app).get("/api/items/1");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(
       expect.objectContaining({
         name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
         price: 109.95,
