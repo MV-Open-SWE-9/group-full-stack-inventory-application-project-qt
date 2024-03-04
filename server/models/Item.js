9a2c7dbd9c5645779c5fe48f10269f3cafae6616
Alex Evans <aevans@quiktrip.com>
2024-03-04 12:59:23 -0600

    Update Item.js
    Updated the Item model to include five items; Name, Price, Description, Category, and Image.
    
    -Item model was also exported and connected tot he database

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,2 +1,10 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const db = new Sequelize("");
+const { Sequelize, db } = require("../db");
+
+const Item = db.define("item", {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  category: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
+module.exports = Item;
