073e8c19899807ba19a5993f28b57a2747333b13
Alex Evans <157507835+AlexanderEvans23@users.noreply.github.com>
2024-03-04 16:46:30 -0500

    Test (#22)
    * Add get all test
    
    add test to confirm that GET all item request works and returns properly
    
    * Update Item.js
    
    Add test to test get request for singular item
    
    * Update item.test.js
    
    add test to test the post request
    
    * Update item.test.js
    
    add test functionality to test delete request

--- server/seed.js
+++ server/seed.js
@@ -1,22 +1,22 @@
-const {sauces, items} = require('./seedData.js');
+const { sauces, items } = require("./seedData.js");
 
-const {db} = require('./db');
-const {Item} = require('./models');
-
+const { db } = require("./db");
+const { Item } = require("./models");
 
 const seed = async () => {
+  try {
+    // drop and recreate tables per model definitions
+    await db.sync({ force: true });
 
-    try {
-        // drop and recreate tables per model definitions
-        await db.sync({ force: true });
-    
-        // insert data
-        await Promise.all(items.map(item => Item.create(item)));
+    // insert data
+    await Promise.all(items.map((item) => Item.create(item)));
 
-        console.log("db populated!");
-    } catch (error) {
-        console.error(error);
-    }
-}
+    //console.log("db populated!");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 seed();
+
+module.exports = seed;
