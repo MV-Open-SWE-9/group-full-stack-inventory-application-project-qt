70e5d4be62a23fa6acf4ceec16e8b074db358eda
Alex Evans <aevans@quiktrip.com>
2024-03-04 15:16:17 -0600

    Add get all test
    add test to confirm that GET all item request works and returns properly

---
+++ server/items.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const app = require("./app");
+const Item = require("./models/Item");
+const seed = require("./seed");
+
+describe("Item Tests", () => {
+  beforeAll(async () => {
+    await seed();
+  });
+  test("get users returns correct response", async () => {
+    const response = await request(app).get("/api/items");
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0]).toEqual(
+      expect.objectContaining({
+        name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        price: 109.95,
+        description:
+          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        category: "men's clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      })
+    );
+  });
+});
