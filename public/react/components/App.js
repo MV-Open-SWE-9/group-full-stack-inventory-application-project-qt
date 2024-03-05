5f67d74e66685dc07e09cf2e6d285634cd3fc707
emartincek1 <evan.martincek@gmail.com>
2024-03-05 09:05:54 -0600

    create all items front-end view

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import React, { useState, useEffect } from "react";
+import { SaucesList } from "./SaucesList";
+import Item from "./Item";
+import { Container, Row, Col } from "react-bootstrap";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
+  // const [sauces, setSauces] = useState([]);
 
-	const [sauces, setSauces] = useState([]);
+  // async function fetchSauces(){
+  // 	try {
+  // 		const response = await fetch(`${apiURL}/sauces`);
+  // 		const saucesData = await response.json();
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  // 		setSauces(saucesData);
+  // 	} catch (err) {
+  // 		console.log("Oh no an error! ", err)
+  // 	}
+  // }
 
-	useEffect(() => {
-		fetchSauces();
-	}, []);
+  // useEffect(() => {
+  // 	fetchSauces();
+  // }, []);
 
-	return (
-		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
-		</main>
-	)
-}
+  // return (
+  // 	<main>
+  //   <h1>Sauce Store</h1>
+  // 		<h2>All things 🔥</h2>
+  // 		<SaucesList sauces={sauces} />
+  // 	</main>
+  // )
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const res = await fetch(`${apiURL}/items`);
+      const data = await res.json();
+      setItems(data);
+    } catch (e) {
+      console.log("Error", e);
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        {items.map((item) => (
+          <Item item={item} />
+        ))}
+      </Row>
+    </Container>
+  );
+};
