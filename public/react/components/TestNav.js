9c57a55cd9a3e128ae5a3ad01976d65e2f4986cb
Alex Evans <aevans@quiktrip.com>
2024-03-06 07:56:46 -0600

    add styles
    add styles to navbar

--- public/react/components/TestNav.js
+++ public/react/components/TestNav.js
@@ -3,9 +3,9 @@ import { Container, Nav, Navbar, Button } from "react-bootstrap";
 
 function TestNav({ setCreating }) {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary mb-5">
+    <Navbar expand="lg" className="mb-5" bg="primary">
       <Container>
-        <Navbar.Brand>Inventory App</Navbar.Brand>
+        <Navbar.Brand bg="light">Inventory App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Button onClick={() => setCreating(true)}>Add New Item</Button>
       </Container>
